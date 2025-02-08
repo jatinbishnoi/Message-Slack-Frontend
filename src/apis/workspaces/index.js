7440831d5a6bec96665c0f36189a5161c0ac9be2
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-02-08 13:26:37 +0530

    Added mutation for delete and update workspace

--- src/apis/workspaces/index.js
+++ src/apis/workspaces/index.js
@@ -1,4 +1,3 @@
-
 import axios from '@/config/axiosConfig';
 
 export const createWorkspaceRequest = async ({ name, description, token }) => {
@@ -45,6 +44,35 @@ export const fetchWorkspaceDetailsRequest = async ({ workspaceId, token }) => {
         return response?.data?.data;
     } catch(error) {
         console.log('Error in fetching workspace details request', error);
+        throw error.response.data;
+    }
+};
+
+export const deleteWorkspaceRequest = async ({ workspaceId, token}) => {
+    try {
+        const response = await axios.delete(`/workspaces/${workspaceId}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+
+        return response?.data?.data;
+    } catch(error) {
+        console.log('Error in deleting workspace request', error);
+        throw error.response.data;
+    }
+};
+
+export const updateWorkspaceRequest = async ({ workspaceId, name, token}) => {
+    try {
+        const response = await axios.put(`/workspaces/${workspaceId}`, { name }, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        return response?.data?.data;
+    } catch(error) {
+        console.log('Error in updating workspace request', error);
         throw error.response.data;
     }
 };
--- src/components/molecules/Workspace/WorkspacePreferencesModal.jsx
+++ src/components/molecules/Workspace/WorkspacePreferencesModal.jsx
@@ -38,6 +38,12 @@ export const WorkspacePreferencesModal = () => {
                             </p>
                         </div>
 
+                        <p
+                            className='text-sm'
+                        >
+                            {initialValue}
+                        </p>
+
 
 
                     </div>
@@ -46,7 +52,9 @@ export const WorkspacePreferencesModal = () => {
                         className='flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50'
                     >
                         <TrashIcon className='size-5' />
-                        <p>
+                        <p
+                            className='text-sm font-semibold'
+                        >
                             Delete Workspace
                         </p>
                     </button>
