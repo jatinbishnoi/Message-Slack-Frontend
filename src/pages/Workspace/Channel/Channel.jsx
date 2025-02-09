78342ce3d2fc7f84e458808edc7d7ad6ceb0b9bf
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-02-09 14:01:05 +0530

    Added css for quill

--- src/pages/Workspace/Channel/Channel.jsx
+++ src/pages/Workspace/Channel/Channel.jsx
@@ -1,9 +1,39 @@
+import { Loader2Icon, TriangleAlertIcon } from 'lucide-react';
 import { useParams } from 'react-router-dom';
+
+import { ChatInput } from '@/components/molecules/ChatInput/ChatInput';
+import { useGetChannelById } from '@/hooks/apis/channels/useGetChannelById';
+
 export const Channel = () => {
+
     const { channelId } = useParams();
+
+    // eslint-disable-next-line no-unused-vars
+    const { channelDetails, isFetching, isError } = useGetChannelById(channelId);
+
+    if(isFetching) {
+        return (
+            <div
+                className='h-full flex-1 flex items-center justify-center'
+            >
+                <Loader2Icon className='size-5 animate-spin text-muted-foreground' />
+            </div>
+        );
+    }
+
+    if(isError) {
+        return (
+            <div className='h-full flex-1 flex flex-col gap-y-2 items-center justify-center'>
+                <TriangleAlertIcon className='size-6 text-muted-foreground' />
+                <span className='text-sm text-muted-foreground'>Channel Not found</span>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            Channel {channelId}
+        <div className='flex flex-col h-full'>
+            <div className='flex-1' />
+            <ChatInput />
         </div>
     );
 };
